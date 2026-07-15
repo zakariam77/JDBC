47bf56218c97110d88f06d625dde0e7b4a1f3f23
zakariam77 <zakariamoujahid90@gmail.com>
2026-07-15 17:49:13 +0100

    nvn

--- src/test/java/com/zproject/UltimateExampleTest.java
+++ src/test/java/com/zproject/UltimateExampleTest.java
@@ -36,13 +36,13 @@ public class UltimateExampleTest extends Basetest{
     @DataProvider(name = "dataProvider" , parallel = true)
     public Iterator<Object[]> dataProvider() throws SQLException {
 
-        String url = "jdbc:mysql://localhost:3306/jdbc";
-        String rootUsername = "root";
-        String rootPassword = "root";
+        String url = "jdbc:mysql://localhost:3306/newdb";
+        String rootUsername = "zakaria";
+        String rootPassword = "Azerty----@2";
 
         Connection conn = DriverManager.getConnection(url, rootUsername, rootPassword);
         Statement statement = conn.createStatement();
-        ResultSet rs = statement.executeQuery("select username, passwords from info2");
+        ResultSet rs = statement.executeQuery("select username, passwords from info");
         List<Object[]> dataList = new ArrayList<>();
 
         while (rs.next()){
