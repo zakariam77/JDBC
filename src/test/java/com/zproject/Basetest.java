fbadd40c5d4ffd5b136929170131397717cadea2
zakariam77 <zakariamoujahid90@gmail.com>
2026-07-16 14:57:10 +0100

    nb

--- src/test/java/com/zproject/Basetest.java
+++ src/test/java/com/zproject/Basetest.java
@@ -40,6 +40,7 @@ public class Basetest {
         //temporary testing remote webdriver to docker compose
         logger.info("instantiating chrome driver");
         WebDriver driver = new RemoteWebDriver(new URI("http://172.19.0.2:4444").toURL(), options);
+        logger.info(Thread.currentThread().getName());
         DriverManage.setDriver(driver);
     }
    @AfterMethod
